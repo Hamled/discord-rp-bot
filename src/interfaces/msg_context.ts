e6cf2419eb801500c596aa9b922959843fd19f01
Charles Ellis <hamled@hamled.name>
2019-10-24 16:10:01 -0700

    Message context can hold session recorder

--- src/interfaces/msg_context.ts
+++ src/interfaces/msg_context.ts
@@ -1,7 +1,9 @@
 import {TextChannel} from '../interfaces';
 import {Session} from '../models';
+import {SessionRecorder} from './session_recorder';
 
 export interface MsgContext {
   channel: TextChannel;
   session?: Session;
+  recorder?: SessionRecorder;
 }
