40133227c5b4f8e1c461f431023102b5b0cd9e9c
Charles Ellis <hamled@hamled.name>
2019-10-24 16:00:28 -0700

    EntityManager test fake supports saving arrays
    This is functionality already allowed by the TypeORM implementation of
    EntityManager, and will be used by the session recorder.

--- test/fakes/entity_manager.ts
+++ test/fakes/entity_manager.ts
@@ -7,10 +7,18 @@ export class FakeEntityManager extends EntityManager {
     super((null as unknown) as Connection);
   }
 
-  save(entity: any): Promise<any> {
-    this.savedEntities.push(entity);
+  save(entity: any | any[]): Promise<any> {
+    if(Array.isArray(entity)) {
+      entity.forEach(e => this.saveInternal(e));
+    } else {
+      this.saveInternal(entity);
+    }
 
-    entity.id = this.savedEntities.length;
     return entity;
+  }
+
+  private saveInternal(entity: any): void {
+    this.savedEntities.push(entity);
+    entity.id = this.savedEntities.length;
   }
 }
