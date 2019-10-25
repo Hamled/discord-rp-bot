1089ba80c616ef19df8055e7825c17635988d21b
Charles Ellis <hamled@hamled.name>
2019-10-24 17:20:52 -0700

    Reorganize StartCommand#process tests
    The diff for this looks complicated, but I only re-arranged the tests
    into two sub-categories for when a session is present and when it is
    not.
    
    A couple of tests were left in the top-level "describe" block for this
    function, because they should apply in both cases. However, most
    behavior depends on whether there is an active session for the context's
    channel or not, so this organization makes more sense.

--- test/bot/commands/start.test.ts
+++ test/bot/commands/start.test.ts
@@ -37,40 +37,6 @@ describe('start command', () => {
   });
 
   describe('process', () => {
-    it('should save a new session', async () => {
-      expect.assertions(3);
-      const numSavedEntities = manager.savedEntities.length;
-
-      await cmd.process(new FakeMsgContext());
-
-      expect(Session).toHaveBeenCalledTimes(1);
-      expect(manager.savedEntities.length).toBe(numSavedEntities + 1);
-      expect(manager.savedEntities[numSavedEntities]).toBeInstanceOf(Session);
-    });
-
-    it('should set the new session\'s channel ID from the context', async () => {
-      expect.assertions(1);
-      const channelId = '12345';
-
-      await cmd.process(new FakeMsgContext(channelId));
-
-      const session = SessionMock.mock.instances[0];
-      expect(session.channelId).toEqual(channelId);
-    });
-
-    it('should start a new session recorder', async () => {
-      expect.assertions(2);
-
-      const context = new FakeMsgContext();
-      await cmd.process(context);
-
-      const session = SessionMock.mock.instances[0];
-      expect(SessionRecorderMock).toHaveBeenCalledWith(session, context.channel, manager);
-
-      const recorder = SessionRecorderMock.mock.results[0].value;
-      expect(recorder.start).toHaveBeenCalled();
-    });
-
     it('should return a context with the new session', async () => {
       expect.assertions(1);
       const newContext = await cmd.process(new FakeMsgContext());
@@ -87,29 +53,67 @@ describe('start command', () => {
       expect(newContext.recorder).toBe(recorder);
     });
 
-    it('should not create a new session if one already exists', async () => {
-      expect.assertions(3);
-      const context = new FakeMsgContext('12345', fakeSession);
-      const numSavedEntities = manager.savedEntities.length;
+    describe('when session does not exist', () => {
+      it('should save a new session', async () => {
+        expect.assertions(3);
+        const numSavedEntities = manager.savedEntities.length;
 
-      const newContext = await cmd.process(context);
+        await cmd.process(new FakeMsgContext());
 
-      expect(Session).toHaveBeenCalledTimes(0);
-      expect(manager.savedEntities.length).toBe(numSavedEntities);
-      expect(newContext.session).toBe(fakeSession);
+        expect(Session).toHaveBeenCalledTimes(1);
+        expect(manager.savedEntities.length).toBe(numSavedEntities + 1);
+        expect(manager.savedEntities[numSavedEntities]).toBeInstanceOf(Session);
+      });
+
+      it('should set the new session\'s channel ID from the context', async () => {
+        expect.assertions(1);
+        const channelId = '12345';
+
+        await cmd.process(new FakeMsgContext(channelId));
+
+        const session = SessionMock.mock.instances[0];
+        expect(session.channelId).toEqual(channelId);
+      });
+
+      it('should start a new session recorder', async () => {
+        expect.assertions(2);
+
+        const context = new FakeMsgContext();
+        await cmd.process(context);
+
+        const session = SessionMock.mock.instances[0];
+        expect(SessionRecorderMock).toHaveBeenCalledWith(session, context.channel, manager);
+
+        const recorder = SessionRecorderMock.mock.results[0].value;
+        expect(recorder.start).toHaveBeenCalled();
+      });
     });
 
-    it('should not create a session recorder if session already exists', async () => {
-      expect.assertions(2);
-      const mockRecorder = new SessionRecorder(fakeSession, new FakeTextChannel(), manager);
-      SessionRecorderMock.mockClear();
+    describe('when session already exists', () => {
+      it('should not create a new session', async () => {
+        expect.assertions(3);
+        const context = new FakeMsgContext('12345', fakeSession);
+        const numSavedEntities = manager.savedEntities.length;
 
-      const context = new FakeMsgContext('12345', fakeSession, mockRecorder);
+        const newContext = await cmd.process(context);
 
-      const newContext = await cmd.process(context);
+        expect(Session).toHaveBeenCalledTimes(0);
+        expect(manager.savedEntities.length).toBe(numSavedEntities);
+        expect(newContext.session).toBe(fakeSession);
+      });
 
-      expect(SessionRecorderMock).not.toHaveBeenCalled();
-      expect(newContext.recorder).toBe(mockRecorder);
+      it('should not create a session recorder if one already exists', async () => {
+        expect.assertions(2);
+        const mockRecorder = new SessionRecorder(fakeSession, new FakeTextChannel(), manager);
+        SessionRecorderMock.mockClear();
+
+        const context = new FakeMsgContext('12345', fakeSession, mockRecorder);
+
+        const newContext = await cmd.process(context);
+
+        expect(SessionRecorderMock).not.toHaveBeenCalled();
+        expect(newContext.recorder).toBe(mockRecorder);
+      });
     });
   })
 });
