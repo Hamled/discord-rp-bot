4e1969268f961609387e981407307ec1381bb6b3
Charles Ellis <hamled@hamled.name>
2019-10-24 17:04:10 -0700

    MsgContext test fake can hold session recorder

--- test/fakes/msg_context.ts
+++ test/fakes/msg_context.ts
@@ -1,13 +1,16 @@
 import {MsgContext, TextChannel} from '../../src/interfaces';
 import {Session} from '../../src/models/session';
+import {SessionRecorder} from '../../src/bot/session_recorder';
 import {FakeTextChannel} from './text_channel';
 
 export class FakeMsgContext implements MsgContext {
   channel: TextChannel;
   session?: Session;
+  recorder?: SessionRecorder;
 
-  constructor(channelId?: string, session?: Session) {
+  constructor(channelId?: string, session?: Session, recorder?: SessionRecorder) {
     this.channel = new FakeTextChannel(channelId),
     this.session = session;
+    this.recorder = recorder;
   }
 }
